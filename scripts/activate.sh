ed563ce24b551b4f90eb6b6c1a90bf9ee4819da6
Stefan Zabka <zabka@campus.tu-berlin.de>
2022-05-06 21:26:05 +0200

    chore(esp-idf): set up esp-idf as a third-party dependency
    Closes #10

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 git submodule update --recursive
+
+pushd thirdparty/esp-idf/repo || exit 1
+git checkout v4.4.1
+git submodule update --init
+./install.sh
+. ./export.sh
+popd || exit 1
+
 # call connectedhomeip setup script
 pushd thirdparty/chip/repo || exit 1
 source ./scripts/activate.sh
