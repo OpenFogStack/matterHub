cca997180ebd6b9f41035da6a41df967916f700c
Jan Golebiowski <golebiowski@campus.tu-berlin.de>
2022-06-01 17:03:49 +0200

    fixed Memory Error

--- src/main/include/ReportCommand.h
+++ src/main/include/ReportCommand.h
@@ -5,7 +5,7 @@
 #pragma once
 
 #include "BindingHandler.h"
-#include <app/tests/suites/commands/interaction_model/InteractionModel.h>
+#include <InteractionModel.h>
 
 class ReportCommand : public InteractionModelReports,
                       public chip::app::ReadClient::Callback {
