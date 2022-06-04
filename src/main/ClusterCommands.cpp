29f233b20eef1bd6f2e88d47b403f753486d9201
Maximilian Weisenseel <weisenseel@campus.tu-berlin.de>
2022-06-04 15:03:35 +0200

    WIP: Implemented Shell structure
    Signed-off-by: Maximilian Weisenseel <weisenseel@campus.tu-berlin.de>

---
+++ src/main/ClusterCommands.cpp
@@ -0,0 +1,241 @@
+/*
+ *
+ *    Copyright (c) 2022 Project CHIP Authors
+ *    All rights reserved.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+#include "app/CommandSender.h"
+#include "app/clusters/bindings/BindingManager.h"
+#include "app/server/Server.h"
+#include "controller/InvokeInteraction.h"
+#include "platform/CHIPDeviceLayer.h"
+#include <app/clusters/bindings/bindings.h>
+#include <lib/support/CodeUtils.h>
+
+#if CONFIG_ENABLE_CHIP_SHELL
+#include "lib/shell/Engine.h"
+#include "lib/shell/commands/Help.h"
+#endif // ENABLE_CHIP_SHELL
+
+using namespace chip;
+using namespace chip::app;
+
+#if CONFIG_ENABLE_CHIP_SHELL
+using Shell::Engine;
+using Shell::shell_command_t;
+using Shell::streamer_get;
+using Shell::streamer_printf;
+
+Engine sShellClusterSubCommands;
+Engine sShellClusterOnOffSubCommands;
+
+#endif // defined(ENABLE_CHIP_SHELL)
+
+static const char *TAG = "Cluster Commands";
+namespace
+{
+
+/*
+    void ProcessOnOffUnicastBindingCommand(CommandId commandId, const EmberBindingTableEntry &binding, DeviceProxy *peer_device)
+    {
+        auto onSuccess = [](const ConcreteCommandPath &commandPath, const StatusIB &status, const auto &dataResponse)
+        {
+            ChipLogProgress(NotSpecified, "OnOff command succeeds");
+        };
+
+        auto onFailure = [](CHIP_ERROR error)
+        {
+            ChipLogError(NotSpecified, "OnOff command failed: %" CHIP_ERROR_FORMAT, error.Format());
+        };
+
+        switch (commandId)
+        {
+        case Clusters::OnOff::Commands::Toggle::Id:
+            Clusters::OnOff::Commands::Toggle::Type toggleCommand;
+            Controller::InvokeCommandRequest(peer_device->GetExchangeManager(), peer_device->GetSecureSession().Value(), binding.remote,
+                                             toggleCommand, onSuccess, onFailure);
+            break;
+
+        case Clusters::OnOff::Commands::On::Id:
+            Clusters::OnOff::Commands::On::Type onCommand;
+            Controller::InvokeCommandRequest(peer_device->GetExchangeManager(), peer_device->GetSecureSession().Value(), binding.remote,
+                                             onCommand, onSuccess, onFailure);
+            break;
+
+        case Clusters::OnOff::Commands::Off::Id:
+            Clusters::OnOff::Commands::Off::Type offCommand;
+            Controller::InvokeCommandRequest(peer_device->GetExchangeManager(), peer_device->GetSecureSession().Value(), binding.remote,
+                                             offCommand, onSuccess, onFailure);
+            break;
+        }
+    }
+
+    void LightSwitchChangedHandler(const EmberBindingTableEntry &binding, DeviceProxy *peer_device, void *context)
+    {
+        VerifyOrReturn(context != nullptr, ChipLogError(NotSpecified, "OnDeviceConnectedFn: context is null"));
+        BindingCommandData *data = static_cast<BindingCommandData *>(context);
+
+        if (binding.type == EMBER_MULTICAST_BINDING && data->isGroup)
+        {
+            switch (data->clusterId)
+            {
+            case Clusters::OnOff::Id:
+                ProcessOnOffGroupBindingCommand(data->commandId, binding);
+                break;
+            }
+        }
+        else if (binding.type == EMBER_UNICAST_BINDING && !data->isGroup)
+        {
+            switch (data->clusterId)
+            {
+            case Clusters::OnOff::Id:
+                ProcessOnOffUnicastBindingCommand(data->commandId, binding, peer_device);
+                break;
+            }
+        }
+    }
+
+    void InitBindingHandlerInternal(intptr_t arg)
+    {
+        auto &server = chip::Server::GetInstance();
+        chip::BindingManager::GetInstance().Init(
+            {&server.GetFabricTable(), server.GetCASESessionManager(), &server.GetPersistentStorage()});
+        chip::BindingManager::GetInstance().RegisterBoundDeviceChangedHandler(LightSwitchChangedHandler);
+    }
+*/
+#ifdef CONFIG_ENABLE_CHIP_SHELL
+
+    /********************************************************
+     * Cluster shell functions
+     *********************************************************/
+
+    CHIP_ERROR ClusterHelpHandler(int argc, char **argv)
+    {
+        sShellClusterSubCommands.ForEachCommand(Shell::PrintCommandHelp, nullptr);
+        return CHIP_NO_ERROR;
+    }
+
+    CHIP_ERROR ClusterCommandHandler(int argc, char **argv)
+    {
+        if (argc == 0)
+        {
+            return ClusterHelpHandler(argc, argv);
+        }
+
+        return sShellClusterSubCommands.ExecCommand(argc, argv);
+    }
+
+    /********************************************************
+     * OnOff switch shell functions
+     *********************************************************/
+
+    CHIP_ERROR ClusterCommandOnOffHelpHandler(int argc, char **argv)
+    {
+        sShellClusterOnOffSubCommands.ForEachCommand(Shell::PrintCommandHelp, nullptr);
+        return CHIP_NO_ERROR;
+    }
+
+    CHIP_ERROR ClusterCommandOnOffHandler(int argc, char **argv)
+    {
+        if (argc == 0)
+        {
+            return ClusterCommandOnOffHelpHandler(argc, argv);
+        }
+
+        return sShellClusterOnOffSubCommands.ExecCommand(argc, argv);
+    }
+
+    CHIP_ERROR ClusterCommandOnOffOnHandler(int argc, char **argv)
+    {
+        ESP_LOGI(TAG, "On Handler -> TODO");
+        /*BindingCommandData *data = Platform::New<BindingCommandData>();
+        data->commandId = Clusters::OnOff::Commands::On::Id;
+        data->clusterId = Clusters::OnOff::Id;
+
+        DeviceLayer::PlatformMgr().ScheduleWork(ClusterCommandWorkerFunction, reinterpret_cast<intptr_t>(data));
+        */
+        return CHIP_NO_ERROR;
+    }
+
+    CHIP_ERROR ClusterCommandOnOffOffHandler(int argc, char **argv)
+    {
+        ESP_LOGI(TAG, "Off Handler -> TODO");
+        /*
+        BindingCommandData *data = Platform::New<BindingCommandData>();
+        data->commandId = Clusters::OnOff::Commands::Off::Id;
+        data->clusterId = Clusters::OnOff::Id;
+
+        DeviceLayer::PlatformMgr().ScheduleWork(ClusterCommandWorkerFunction, reinterpret_cast<intptr_t>(data));
+        */
+        return CHIP_NO_ERROR;
+    }
+
+    CHIP_ERROR ClusterCommandOnOffToggleHandler(int argc, char **argv)
+    {
+        ESP_LOGI(TAG, "Toggle Handler -> TODO");
+        /*
+        BindingCommandData *data = Platform::New<BindingCommandData>();
+        data->commandId = Clusters::OnOff::Commands::Toggle::Id;
+        data->clusterId = Clusters::OnOff::Id;
+
+        DeviceLayer::PlatformMgr().ScheduleWork(ClusterCommandWorkerFunction, reinterpret_cast<intptr_t>(data));
+        */
+        return CHIP_NO_ERROR;
+    }
+
+    /**
+     * @brief configures switch matter shell
+     *
+     */
+
+#endif // ENABLE_CHIP_SHELL
+
+} // namespace
+
+/********************************************************
+ * Switch functions
+ *********************************************************/
+/*
+void ClusterCommandWorkerFunction(intptr_t context)
+{
+    VerifyOrReturn(context != 0, ChipLogError(NotSpecified, "SwitchWorkerFunction - Invalid work data"));
+
+    BindingCommandData *data = reinterpret_cast<BindingCommandData *>(context);
+    BindingManager::GetInstance().NotifyBoundClusterChanged(data->localEndpointId, data->clusterId, static_cast<void *>(data));
+
+    Platform::Delete(data);
+}
+*/
+void RegisterClusterCommands()
+{
+
+    static const shell_command_t sClusterSubCommands[] = {
+        {&ClusterHelpHandler, "help", "Usage: cluster <subcommand>"},
+        {&ClusterCommandOnOffHandler, "onoff", " Usage: cluster onoff <subcommand>"},
+    };
+
+    static const shell_command_t sClusterCommandOnOffSubCommands[] = {
+        {&ClusterCommandOnOffHelpHandler, "help", "Usage : cluster ononff <subcommand>"},
+        {&ClusterCommandOnOffOnHandler, "on", "Usage: cluster onoff on <Fabric> <Node> <Endpoint>"},
+        {&ClusterCommandOnOffOffHandler, "off", "Usage: cluster onoff off <Fabric> <Node> <Endpoint>"},
+        {&ClusterCommandOnOffToggleHandler, "toggle", "Usage: cluster onoff off <Fabric> <Node> <Endpoint>"}};
+
+    static const shell_command_t sClusterCommand = {&ClusterCommandHandler, "cluster",
+                                                    "Cluster commands. Usage: cluster <subcommand>"};
+
+    sShellClusterOnOffSubCommands.RegisterCommands(sClusterCommandOnOffSubCommands, ArraySize(sClusterCommandOnOffSubCommands));
+    sShellClusterSubCommands.RegisterCommands(sClusterSubCommands, ArraySize(sClusterSubCommands));
+
+    Engine::Root().RegisterCommands(&sClusterCommand, 1);
+}
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -29,13 +29,14 @@
 #include "freertos/task.h"
 #include "nvs_flash.h"
 #include "shell_extension/launch.h"
+#include "ClusterCommands.h"
 
 #include <app/server/OnboardingCodesUtil.h>
 
 using namespace ::chip;
 using namespace ::chip::DeviceManager;
 
-static const char * TAG = "light-switch-app";
+static const char *TAG = "light-switch-app";
 
 static AppDeviceCallbacks EchoCallbacks;
 
@@ -65,9 +66,10 @@ extern "C" void app_main()
 
 #if CONFIG_ENABLE_CHIP_SHELL
     chip::LaunchShell();
+    RegisterClusterCommands();
 #endif // CONFIG_ENABLE_CHIP_SHELL
 
-    CHIPDeviceManager & deviceMgr = CHIPDeviceManager::GetInstance();
+    CHIPDeviceManager &deviceMgr = CHIPDeviceManager::GetInstance();
 
     CHIP_ERROR error = deviceMgr.Init(&EchoCallbacks);
     if (error != CHIP_NO_ERROR)
